4fd0fd1dc4d3cfbebe942528299f6e8bc86e4117
giorgosanastasiou <giorgos.anastasiou172@gmail.com>
2022-06-05 03:11:28 +0300

    more updates

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from wtforms import SubmitField
 import yaml
 from logging import FileHandler,WARNING
 import markups
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
@@ -43,6 +44,9 @@ app.config['MYSQL_USER'] = db['mysql_user']
 app.config['MYSQL_PASSWORD'] = db['mysql_password']
 app.config['MYSQL_DB'] = db['mysql_db']
 
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///<db_name>.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 mysql = MySQL()
 mysql.init_app(app)
 
@@ -79,38 +83,39 @@ def index():
 @app.route("/database", methods=['GET', 'POST'])
 def database():
     form = ButtonForm2()
+    return render_template("database.html", form=form)
     if request.method == 'POST':
-        if request.form['submit_button'] == 'Epistimoniko Pedio':
+        if request.form.get('epistimoniko_pedio'):
             return render_template("epistimoniko_pedio.html")
-        elif request.form['submit_button'] == 'Stelexos':
+        elif request.form.get('stelexos'):
             return render_template('stelexos.html')
-        elif request.form['submit_button'] == 'Programma':
+        elif request.form.get('programma'):
             return render_template('programma.html')
-        elif request.form['submit_button'] == 'Organismos':
+        elif request.form.get('organismos'):
             return render_template('organismos.html')
-        elif request.form['submit_button'] == 'Panepistimio':
+        elif request.form.get('panepistimio'):
             return render_template('panepistimio.html')
-        elif request.form['submit_button'] == 'Etairia':
+        elif request.form.get('etairia'):
             return render_template('etairia.html')
-        elif request.form['submit_button'] == 'Ereunitiko Kentro':
+        elif request.form.get('ereunitiko_kentro'):
             return render_template('ereunitiko_kentro.html')
-        elif request.form['submit_button'] == 'Organismos Tilefwna':
+        elif request.form.get('organismos_tilefwna'):
             return render_template('organismos_tilefwna.html')
-        elif request.form['submit_button'] == 'Ereunitis':
+        elif request.form.get('ereunitis'):
             return render_template('ereunitis.html') #####
-        elif request.form['submit_button'] == 'Aksiologisi':
+        elif request.form.get('aksiologisi'):
             return render_template('aksiologisi.html')
-        elif request.form['submit_button'] == 'Ergo':
+        elif request.form.get('ergo'):
             return render_template('ergo.html')
-        elif request.form['submit_button'] == 'Paradoteo':
+        elif request.form.get('paradoteo'):
             return render_template('paradoteo.html')
-        elif request.form['submit_button'] == 'Epistimoniko Pedio Ergou':
+        elif request.form.get('epistimoniko_pedio_ergou'):
             return render_template('epistimoniko_pedio_ergou.html')
-        elif request.form['submit_button'] == 'Ergazetai se Ergo':
+        elif request.form.get('ergazetai_se'):
             return render_template("ergazetai_se_ergo.html")
         #else:
             #pass 
-    elif request.method == 'GET':
+    else:
         return render_template("database.html", form=form)
     #return render_template('database.html') 
 
@@ -125,8 +130,8 @@ def Q1():
             Q1Answer = cur.fetchall()  
             return render_template('Q1.html', Q1Answer = Q1Answer)
         cur.close()
-    #else:
-    #    return render_template('Q1.html')
+    else:
+        return render_template('Q1.html')
 
 
 
@@ -137,13 +142,16 @@ def Q1():
 def Q2():
     if request.method == 'POST':
         cur = mysql.connection.cursor()
-        resultValue = cur.execute(" ")
-        if resultValue > 0:
-            Q2Answer = cur.fetchall()  
-            return render_template('Q2.html', Q2Answer = Q2Answer)
+        resultValue1 = cur.execute(" select * from ereunitis_vw; ")
+        if resultValue1 > 0:
+            Q2A1Answer = cur.fetchall()
+        resultValue2 = cur.execute(" select * from stelexos_vw; ")
+        if resultValue2 > 0 and resultValue1 > 0:
+            Q2A2Answer = cur.fetchall()      
+            return render_template('Q2.html', Q2A1Answer = Q2A1Answer, Q2A2Answer = Q2A2Answer )
         cur.close()
-    #else:
-    #    return render_template('Q2.html')
+    else:
+        return render_template('Q2.html')
 
 
 
@@ -152,13 +160,36 @@ def Q2():
 def Q3():
     if request.method == 'POST':
         cur = mysql.connection.cursor()
-        resultValue = cur.execute(" ")
+        resultValue = cur.execute("select * from epist_pedio")
         if resultValue > 0:
             Q3Answer = cur.fetchall()  
             return render_template('Q3.html', Q3Answer = Q3Answer)
         cur.close()
+    else:
+        return render_template('Q3.html')
+
+
+
+
+
+###
+@app.route("/Q3/Full", methods=['GET', 'POST'])
+def Q3Full():
+    pedio = (request.form['pedio'])
+    if request.method == 'POST':
+        cur = mysql.connection.cursor()
+        resultValue = cur.execute("select e.ergo_id, e.titlos from ergo e inner join epist_pediou_ergou ep on e.ergo_id= ep.ergo_id  where CURRENT_DATE() between e.enarksi and e.liksi and ep.onoma_epist_pediou =  %s;", (pedio,)) ####allagi
+        if resultValue>0:
+            Q3f1Answer = cur.fetchall()  
+        cur.close()
+        cur = mysql.connection.cursor()
+        resultValue2 = cur.execute("select distinct vw.ssn, vw.onoma, vw.epitheto from ereunitis_vw vw inner join (select e.ergo_id, e.titlos from ergo e inner join epist_pediou_ergou ep on e.ergo_id= ep.ergo_id  where CURRENT_DATE() between e.enarksi and e.liksi and ep.onoma_epist_pediou = %s ) s on s.ergo_id = vw.ergo_id;", (pedio,))
+        if resultValue2>0 and resultValue>0:
+            Q3f2Answer = cur.fetchall()
+            return render_template('Q3_full.html', Q3f1Answer = Q3f1Answer, Q3f2Answer = Q3f2Answer)
+        cur.close()
     #else:
-    #    return render_template('Q3.html')
+        #return render_template('Q3_full.html')
 
 
 
@@ -184,13 +215,13 @@ def Q4():
 def Q5():
     if request.method == 'POST':
         cur = mysql.connection.cursor()
-        resultValue = cur.execute(" ")
+        resultValue = cur.execute("select a.onoma_epist_pediou, b.onoma_epist_pediou from epist_pediou_ergou a inner join epist_pediou_ergou b where a.ergo_id = b.ergo_id and a.onoma_epist_pediou < b.onoma_epist_pediou group by a.onoma_epist_pediou, b.onoma_epist_pediou order by count(*) desc limit 3; ")
         if resultValue > 0:
             Q5Answer = cur.fetchall()  
             return render_template('Q5.html', Q5Answer = Q5Answer)
         cur.close()
-    #else:
-    #    return render_template('Q5.html')
+    else:
+        return render_template('Q5.html')
 
 
 
@@ -201,13 +232,13 @@ def Q5():
 def Q6():
     if request.method == 'POST':
         cur = mysql.connection.cursor()
-        resultValue = cur.execute(" ")
+        resultValue = cur.execute("select e.ssn, e.onoma, e.epitheto , count(*) from ereunitis e inner join ergazetai_se_ergo erga on e.ssn = erga.ssn where TIMESTAMPDIFF(YEAR, e.hmeromhnia_gennhshs, CURDATE())<40 group by e.ssn order by count(*) desc limit 3; ")
         if resultValue > 0:
             Q6Answer = cur.fetchall()  
             return render_template('Q6.html', Q6Answer = Q6Answer)
         cur.close()
-    #else:
-    #    return render_template('Q6.html')
+    else:
+        return render_template('Q6.html')
 
 
 
@@ -224,8 +255,8 @@ def Q7():
             Q7Answer = cur.fetchall()  
             return render_template('Q7.html', Q7Answer = Q7Answer)
         cur.close()
-    #else:
-    #    return render_template('Q7.html')
+    else:
+        return render_template('Q7.html')
 
 
 
@@ -235,19 +266,35 @@ def Q7():
 def Q8():
     if request.method == 'POST':
         cur = mysql.connection.cursor()
-        resultValue = cur.execute(" ")
+        resultValue = cur.execute("select ssn, onoma, epitheto, count(*)  from ereunitis_vw where ergo_id not in (select ergo_id from paradoteo) group by ssn having count(*)>4 ;")
         if resultValue > 0:
             Q8Answer = cur.fetchall()  
             return render_template('Q8.html', Q8Answer = Q8Answer)
         cur.close()
-    #else:
-    #    return render_template('Q8.html')
+    else:
+        return render_template('Q8.html')
 
 
 
-    
+'''
 @app.route("/epistimoniko_pedio", methods=['GET', 'POST'])
 def epistimoniko_pedio():
+    if request.method == 'POST':
+        cur = mysql.connection.cursor()
+        resultValue = cur.execute(" ")
+        if resultValue > 0:
+            Answer = cur.fetchall()  
+            return render_template('epistimoniko_pedio.html', '0')
+        cur.close()
+        #return render_template('Q7.html')
+    elif request.method == 'GET':
+        return render_template('epistimoniko_pedio.html', '0')
+
+
+'''
+'''
+@app.route('/database/<variable>/create')
+def create(variable):
     if request.method == 'POST':
         cur = mysql.connection.cursor()
         resultValue = cur.execute(" ")
@@ -258,37 +305,63 @@ def epistimoniko_pedio():
         #return render_template('Q7.html')
     elif request.method == 'GET':
         return render_template('epistimoniko_pedio.html')
+    
+'''
+
+
+@app.route('/database/<variable>/retrieve', methods = ['GET', 'POST'])
+def retrieve(variable):
+    if request.method == 'POST' or request.method == 'GET':
+        cur = mysql.connection.cursor()
+        resultValue = cur.execute(" select * from %s;", (variable,))
+        if resultValue > 0:
+            Answer = cur.fetchall()  
+            return render_template('epistimoniko_pedio.html', Answer = Answer)
+        cur.close()
+        #return render_template('Q7.html')
+    else: #request.method == 'GET':
+        Answer = 0
+        return render_template('epistimoniko_pedio.html', Answer = Answer)   
+
+
+'''
+@app.route('/database/<variable>/update')
+def update(variable):
+    
+
+
+
+@app.route('/database/<variable>/delete')
+def delete(variable):
+    
+'''
+
 
 
 
 @app.route("/database/epistimoniko_pedio", methods=['GET', 'POST'])
 def database_epistimoniko_pedio():
-    return render_template('<section>.html')
+    Answer = 0
+    return render_template('epistimoniko_pedio.html', Answer = Answer)
 
 
 
 @app.route("/database/stelexos", methods=['GET', 'POST'])
-def database_epistimoniko_pedio():
-    assert section == request.view_args['section']
-    #if section == 'organismos_tilefwna':
-    return render_template('<section>.html')
+def database_stelexos():
+    return render_template('stelexos.html')
 
 
 
 
 @app.route("/database/programma", methods=['GET', 'POST'])
-def database_epistimoniko_pedio():
-    assert section == request.view_args['section']
-    #if section == 'organismos_tilefwna':
-    return render_template('<section>.html')
+def database_programma():
+    return render_template('programma.html')
 
 
 
 
 @app.route("/database/organismos", methods=['GET', 'POST'])
 def database_organismos():
-    assert section == request.view_args['section']
-    #if section == 'organismos_tilefwna':
     return render_template('organismos.html')
 
 
@@ -297,15 +370,13 @@ def database_organismos():
 
 @app.route("/database/panepistimio", methods=['GET', 'POST'])
 def database_panepistimio():
-
+    return render_template('panepistimio.html')
 
 
 
 
 @app.route("/database/etairia", methods=['GET', 'POST'])
-def database_epistimoniko_pedio():
-    assert section == request.view_args['section']
-    #if section == 'organismos_tilefwna':
+def database_etairia():
     return render_template('etairia.html')
 
 
@@ -314,8 +385,6 @@ def database_epistimoniko_pedio():
 
 @app.route("/database/ereunitiko_kentro", methods=['GET', 'POST'])
 def database_ereunitiko_kentro():
-    assert section == request.view_args['section']
-    #if section == 'organismos_tilefwna':
     return render_template('ereunitiko_kentro.html')
 
 
@@ -323,66 +392,52 @@ def database_ereunitiko_kentro():
 
 
 @app.route("/database/organismos_tilefwna", methods=['GET', 'POST'])
-def database(section):
-    assert section == request.view_args['section']
-    #if section == 'organismos_tilefwna':
-    return render_template('<section>.html')
+def database_organismos_tilefwna():
+    return render_template('organismos_tilefwna.html')
 
 
 
 
 
 @app.route("/database/ereunitis", methods=['GET', 'POST'])
-def database(section):
-    assert section == request.view_args['section']
-    #if section == 'organismos_tilefwna':
-    return render_template('<section>.html')
+def database_ereunitis():
+    return render_template('ereunitis.html')
 
 
 
 
 
 @app.route("/database/aksiologisi", methods=['GET', 'POST'])
-def database(section):
-    assert section == request.view_args['section']
-    #if section == 'organismos_tilefwna':
-    return render_template('<section>.html')
+def database_aksiologisi():
+    return render_template('aksiologisi.html')
 
 
 
 
 
 @app.route("/database/ergo", methods=['GET', 'POST'])
-def database(section):
-    assert section == request.view_args['section']
-    #if section == 'organismos_tilefwna':
-    return render_template('<section>.html')
+def database_ergo():
+    return render_template('ergo.html')
 
 
 
 
 @app.route("/database/paradoteo", methods=['GET', 'POST'])
-def database(section):
-    assert section == request.view_args['section']
-    #if section == 'organismos_tilefwna':
-    return render_template('<section>.html')
+def database_paradoteo():
+    return render_template('paradoteo.html')
 
 
 
 
 @app.route("/database/epistimoniko_pedio_ergou", methods=['GET', 'POST'])
-def database(section):
-    assert section == request.view_args['section']
-    #if section == 'organismos_tilefwna':
-    return render_template('<section>.html')
+def database_epistimoniko_pedio_ergou():
+    return render_template('epistimoniko_pedio_ergou.html')
 
 
 
 @app.route("/database/ergazetai_se", methods=['GET', 'POST'])
-def database(section):
-    assert section == request.view_args['section']
-    #if section == 'organismos_tilefwna':
-    return render_template('<section>.html')
+def database_ergazetai_se():
+    return render_template('ergazetai_se.html')
 
 
 
